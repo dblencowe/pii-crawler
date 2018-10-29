cedff3fb4d74caf9919d2d89b8404e3b4d49fda0
Dave Blencowe <dblencowe@gmail.com>
2018-10-29 15:32:45 +0000

    Adds plugin setup to Knwl

--- src/Parse.js
+++ src/Parse.js
@@ -3,17 +3,20 @@ const Knwl = require('knwl.js');
 let knwlInstance = null;
 let profile = {};
 
-// let parseHead = ($) => {
-//     $('head').each()
-// };
-
 let setupKnwl = ($) => {
     let lang = null;
     if ($('html').attr('lang')) {
         lang = $('html').attr('lang').replace(/-.*/, '');
         const languages = require('./json/languages');
         console.log(`Setting scrape language to ${languages[lang].name}`);
-        knwlInstance = new Knwl(languages[lang].name);
+        let knwlInstance = new Knwl(languages[lang].name);
+        [
+            { name: 'dates', file: 'default_plugins/dates' },
+            { name: 'links', file: 'default_plugins/links' },
+            { name: 'phones', file: 'default_plugins/phones' }
+        ].forEach((plugin) => {
+            knwlInstance.register(plugin.name, require(`knwl.js/${plugin.file}`));
+        });
     }
 }
 
@@ -21,6 +24,11 @@ module.exports = {
     parse(content) {
         if (! knwlInstance) {
             setupKnwl(content);
+        }
+
+        let items = startElem.getElementsByTagName("*");
+        for (let i = items.length; i--;) {
+            extractFrom(item);
         }
 
         return profile;
